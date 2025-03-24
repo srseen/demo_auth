52a277040534bd3224493386e9ffd384a40831d8
srseen <seereen.2540@gmail.com>
2025-03-24 20:55:32 +0700

    update register API endpoint URL to reflect new port configuration

--- Routes/auth.routes.js
+++ Routes/auth.routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const { register, login } = require("../controllers/auth.controller");
 
-// http://localhost:5000/api/register
+// http://localhost:3000/api/register
 router.post("/register", register);
 router.post("/login", login);
 
